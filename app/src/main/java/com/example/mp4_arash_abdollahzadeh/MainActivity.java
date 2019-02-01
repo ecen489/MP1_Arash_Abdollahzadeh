1b8535c5493b4c9f477b043010045278647bf120
aa75442 <aa75442@tamu.edu>
2019-02-01 06:53:44 -0600

    This is the final version of MP4

---
+++ app/src/main/java/com/example/mp4_arash_abdollahzadeh/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mp4_arash_abdollahzadeh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        final TextView textView = (TextView) findViewById(R.id.txtview);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText());
+                editText.getText().clear();
+            }
+        });
+    }
+
+
+
+
+}
